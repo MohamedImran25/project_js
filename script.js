d37dcb732c3ea763700f2a4b89eaf36a4d36a712
MohamedImran25 <139478403+MohamedImran25@users.noreply.github.com>
2023-08-22 16:44:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function result1(){
+   var x = Number(document.getElementById("fnum").value);
+   var y = Number(document.getElementById("snum").value);
+   var z = x + y;
+   alert(z);
+}
+function result2(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    var z = x - y;
+    alert(z);
+ }
+ function result3(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    var z = x * y;
+    alert(z);
+ }
+ function result4(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    var z = x / y;
+    alert(z);
+ }
